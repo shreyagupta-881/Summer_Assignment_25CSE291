ec0a226d2a5ab1cf1b2707d4e377d977deaa7e07
Shreya Gupta <sg7327316@gmail.com>
2026-06-11 13:05:59 +0530

    Day1 Assignment uploaded

---
+++ Day1/program1.cpp
@@ -0,0 +1,16 @@
+# include <iostream>
+using namespace std;
+int main()
+{int sum=0,n ;
+    cout<<"enter the natural number upto which you need to find sum";
+    cin>> n ;
+for(int i =0;i<=n;i++)
+       {sum=sum+i;
+       }
+       cout<< "sum of natural number"<< sum ;
+cin.get();
+cin.get();
+return 0;
+
+
+}
