e7f2cefd794f0a24a16dcc773109e1ccfa164a00
Shreya Gupta <sg7327316@gmail.com>
2026-06-26 23:09:50 +0530

    Day10 uploaded

---
+++ Day10/prg1.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main(){
+    int i,j,a=0;
+    for(i=1;i<=5;i++){
+        for(j=1;j<=9;j++){
+             printf("*");}
+             printf("\n");
+             a=a+1;
+    }
+return 0;       
+}
