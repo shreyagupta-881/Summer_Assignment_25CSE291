ecc241ebf7594977d2230c8ecbec942921bb85e0
Shreya Gupta <sg7327316@gmail.com>
2026-07-10 20:20:47 +0530

    Day25 uploaded

---
+++ Day25/prg1.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main(){
+    int arr1[]={1,2,3,5,4};
+    int arr2[]={1,2,4,6,7},i;
+    printf("Merge array");
+    for(i=0;i<5;i++)
+    printf("%d",arr1[i]);
+    for(i=0;i<5;i++)
+    printf("%d",arr2[i]);
+    return 0;
+}
